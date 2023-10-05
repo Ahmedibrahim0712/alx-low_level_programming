47bc62decf363c32f3b6aca80f33cf5da52a1c97
MonteBurns1 <zx998@outlook.sa>
2023-10-06 02:48:15 +0300

    asd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-    /* The message to print */
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    /* Length of the message */
-    ssize_t len = 59;
+	/* The message to print */
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	/* Length of the message */
+	ssize_t len = 59;
 
-    /* Use the write system call to print the message to stderr (file descriptor 2) */
-    if (write(2, message, len) != len)
-    {
-        /* Error handling if write fails */
-        return (1);
-    }
+	/* Use the write system call to print the message to stderr (file descriptor 2) */
+	if (write(2, message, len) != len)
+	{
+		/* Error handling if write fails */
+		return (1);
+	}
 
-    return (1);
+	return (1);
 }
