f7281deb75b45a65642dce609b0ae8a1687db403
MonteBurns1 <zx998@outlook.sa>
2023-10-06 02:34:43 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,23 @@
 #include <unistd.h>
+
 /**
- 	* main - this prints exactly the input on it
- * Return: always 1.
+ * main - Entry point of the program
+ *
+ * Return: Always returns 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    ssize_t len = 59; // Length of the message
 
+    // Use the write system call to print the message to stderr (file descriptor 2)
+    ssize_t bytes_written = write(2, message, len);
+
+    if (bytes_written == -1)
+    {
+        // Error handling if write fails
+        return 1;
+    }
+
+    return 0; // Return 0 to indicate successful execution
+}
