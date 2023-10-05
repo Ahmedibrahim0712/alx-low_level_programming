d36f0bcc0121aacd56458c13ac9e6cf51e5802d6
MonteBurns1 <zx998@outlook.sa>
2023-10-06 02:48:58 +0300

    asdf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,23 @@
 #include <unistd.h>
 
 /**
- * main - This prints exactly the input on it.
+ * main - Prints the specified message to stderr.
  *
  * Return: Always returns 1.
  */
 int main(void)
 {
-	/* The message to print */
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	/* Length of the message */
-	ssize_t len = 59;
+    /* The message to print */
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    /* Length of the message */
+    ssize_t len = 59;
 
-	/* Use the write system call to print the message to stderr (file descriptor 2) */
-	if (write(2, message, len) != len)
-	{
-		/* Error handling if write fails */
-		return (1);
-	}
+    /* Use the write system call to print the message to stderr (file descriptor 2) */
+    if (write(2, message, len) != len)
+    {
+        /* Error handling if write fails */
+        return (1);
+    }
 
-	return (1);
+    return (1);
 }
