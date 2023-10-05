25fdc15d24b979b788d1727cc6a1fc62ff1a4578
MonteBurns1 <zx998@outlook.sa>
2023-10-06 02:37:11 +0300

    AS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,23 +1,16 @@
 #include <unistd.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always returns 1.
- */
 int main(void)
 {
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    ssize_t len = 59; // Length of the message
+    ssize_t len = 59;
 
     // Use the write system call to print the message to stderr (file descriptor 2)
-    ssize_t bytes_written = write(2, message, len);
-
-    if (bytes_written == -1)
+    if (write(2, message, len) != len)
     {
         // Error handling if write fails
         return 1;
     }
 
-    return 0; // Return 0 to indicate successful execution
+    return 1;
 }
