fe3c7d8eba4eed34328023418623f7d7fb6f09b3
MonteBurns1 <zx998@outlook.sa>
2023-10-06 03:03:16 +0300

    asff

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,15 +2,15 @@
 
 /**
  * main - Entry point
- * 
- * description: using write function to print a quote
- * 		write(int fd,const void *buf,size_t count)
- * 
+ *
+ * Description: Using the write function to print a quote.
+ *   write(int fd, const void *buf, size_t count)
+ *
  * Return: 1 (not success)
  */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	(write(1, quo, 59);
-		return (1);
+    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, quo, 59);
+    return (1);
 }
