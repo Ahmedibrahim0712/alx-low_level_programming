ea2ccdd3bd62b49ca53120f05a420b1dee09fa2d
MonteBurns1 <zx998@outlook.sa>
2023-10-11 00:20:30 +0300

    asdw

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,30 +8,30 @@ int main(void)
     unsigned long int hold1, hold2, hold3;
     int count;
 
-    // Print the first two Fibonacci numbers
+    /* Print the first two Fibonacci numbers */
     printf("%lu, %lu, ", bk1, bk2);
 
     for (count = 2; count < 98; count++)
     {
         if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
         {
-            // Handle overflow
+            /* Handle overflow */
             hold1 = (bk1 + bk2) / LARGEST;
             hold2 = (bk1 + bk2) % LARGEST;
             hold3 = fr1 + fr2 + hold1;
             fr1 = fr2, fr2 = hold3;
             bk1 = bk2, bk2 = hold2;
 
-            // Print the combined Fibonacci number
+            /* Print the combined Fibonacci number */
             printf("%lu%010lu", fr2, bk2);
         }
         else
         {
-            // No overflow, calculate and print next Fibonacci number
+            /* No overflow, calculate and print the next Fibonacci number */
             hold2 = bk1 + bk2;
             bk1 = bk2, bk2 = hold2;
 
-            // Print the next Fibonacci number
+            /* Print the next Fibonacci number */
             printf("%lu", bk2);
         }
 
@@ -39,7 +39,7 @@ int main(void)
             printf(", ");
     }
 
-    // End with a newline character
+    /* End with a newline character */
     printf("\n");
 
     return (0);
